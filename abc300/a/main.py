d2cd517c6c1011f5b52f9040cae34fe16ff98864
Honahuku <tech@honahuku.com>
2023-07-26 22:35:04 +0900

    fix: abc300/aのdebug対応

--- abc300/a/main.py
+++ abc300/a/main.py
@@ -1,4 +1,9 @@
+import sys
+
+if len(sys.argv) == 2:
+    sys.stdin = open(sys.argv[1])
+
 N, A, B = map(int, input().split())
 C = list(map(int, input().split()))
 
-print(C.index(A+B)+1)
+print(C.index(A + B) + 1)
