37878e3a2807645a219e523ae598265072c1b57d
Honahuku <tech@honahuku.com>
2023-07-30 00:22:18 +0900

    feat: 愚直なソートと比較

--- abc312/c/main.py
+++ abc312/c/main.py
@@ -4,11 +4,27 @@ import sys
 if len(sys.argv) == 2:
     sys.stdin = open(sys.argv[1])
 
-logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+# logging.basicConfig(format="%(message)s", level=logging.DEBUG)
 
 
 def main():
-    logging.debug("hoge")
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    logging.debug(f"{N},{M},{A},{B}")
+    A_sorted = sorted(A)
+    B_sorted = sorted(B, reverse=True)
+    logging.debug(f"{N},{M},{A_sorted},{B_sorted}")
+
+    for i in range(A_sorted[0], B_sorted[0] + 1):
+        sellers = sum(x <= i for x in A_sorted)
+        buyers = sum(x >= i for x in B_sorted)
+        if buyers <= sellers:
+            print(i)
+            return
+
+    print(B_sorted[0] + 1)
 
 
 if __name__ == "__main__":
